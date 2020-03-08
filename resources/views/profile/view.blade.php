3189f89d58e605b1b9345b42ddf94629d3e81a5a
Alaa Akiel <alaa.21.iraq@gmail.com>
2020-03-08 22:43:02 +0300

    update

--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -30,23 +30,23 @@
                 <li>
                     <i class="fa fa-graduation-cap" ></i>
                     <span class="college" >
-                        {{ $user->college->name }}
+                        {{ $user->college ? $user->college->name : 'None' }}
                     </span>
                 </li>
                 <li>
                     <i class="fa fa-briefcase" ></i>
-                    <span class="fields" style="margin-left: 10px;"> 
-                        {{ $user->work ?  $user->work : 'not work yet' }} 
+                    <span class="fields" style="margin-left: 10px;">
+                        {{ $user->work ?  $user->work : 'not work yet' }}
                     </span>
                     @if($auth)
                     <i class="fa fa-pencil-square-o btn-edit" ></i>
                     @endif
                 </li>
                 <li>
-                    <i class="fa  fa-github" ></i> 
+                    <i class="fa  fa-github" ></i>
                     <a name='github' class="fields" href="https://github.com/{{trim($user->github, '@')}}" style="font-size: 14px;margin-left: 25px;" contenteditable="false">
-                        {{ $user->github ?  $user->github : 'None' }} 
-                    </a> 
+                        {{ $user->github ?  $user->github : 'None' }}
+                    </a>
                     @if($auth)
                     <i class="fa fa-pencil-square-o btn-edit" ></i>
                     @endif
@@ -72,35 +72,35 @@
                         <div class="card-body">
                             <div class="d-flex justify-content-between pb-4">
                                 <p class="text">
-                                    
-                                    {{ $project->name }} 
-                                    
-                                    <small style="color:#999595"> 
+
+                                    {{ $project->name }}
+
+                                    <small style="color:#999595">
                                         <i class="fa fa-calendar-minus-o" aria-hidden="true"></i>
-                                        {{ date('Y-m-d', strtotime($project->created_at)) }} 
+                                        {{ date('Y-m-d', strtotime($project->created_at)) }}
                                     </small>
                                 </p>
-                                <a class="float-right" href="/project/{{ $project->id }}/edit"> 
-                                    <i class="fa fa-edit fa-2x"></i> 
+                                <a class="float-right" href="/project/{{ $project->id }}/edit">
+                                    <i class="fa fa-edit fa-2x"></i>
                                 </a>
                             </div>
                             <article>
-                                {{ $project->description }} 
+                                {{ $project->description }}
                                 <a href="{{ $project->link }}">github</a>
                             </article>
-    
+
                             <div class="card-footer"></div>
                         </div>
                         <div class="card-text card-show pt-4 ">
                             @foreach($project->users as $user)
                                 <a href="/user/{{ $user->id }}">{{'@'.$user->fullname}}</a>
                             @endforeach
-                            
+
                         </div>
-                        
+
                     </div>
                 </div>
-                @endforeach 
+                @endforeach
             </div>
         </div>
 
@@ -131,16 +131,16 @@
             padding-right: 1%;
         }
     </style>
-@endsection 
+@endsection
 
 @if($auth)
     @section('script')
         <script>
             $(function(){
                 $('.btn-edit').click(function(event){
-                    $(this).toggleClass('fa-pencil-square-o').toggleClass('fa-check'); 
+                    $(this).toggleClass('fa-pencil-square-o').toggleClass('fa-check');
                     $(this).parent().find('.fields').attr('contenteditable',function(index, attr){
-                        
+
                         if (attr == 'true'){
                             $(this).parent().find('.btn-edit').toggleClass('fa-pencil-square-o').toggleClass('spinner-border');
                             var data  = {}
@@ -148,13 +148,13 @@
                             if ($(this).attr('href')){
                                 $(this).attr('href',"https://github.com/"+evalue.replace("@",'').trim());
                             }
-                            data[$(this).attr('name')] = evalue 
+                            data[$(this).attr('name')] = evalue
                             axios.put('/api/profile',data).then(resp=>{
                                 $(this).parent().find('.btn-edit').toggleClass('fa-pencil-square-o').toggleClass('spinner-border');
                             })
                             return 'false'
                         }
-                        
+
                         return 'true'
                     });
                 })
@@ -171,13 +171,13 @@
                         $('#nav-avatar img').attr('src', e.target.result);
                         df.delete('avatar');
                     }
-                    
+
                     axios.post("/api/profile", df).then(resp=>{
                         reader.readAsDataURL(this.files[0]);
                     })
 
                 });
-            }) 
+            })
         </script>
     @endsection
 @endif
